10a40cd4a8f84c80d9ea3c4c8a33c7f9b3825cd4
alswls07 <sky70614@gmail.com>
2024-06-20 22:23:37 +0900

    MoreButtonEvent

--- EnglishWord.py
+++ EnglishWord.py
@@ -5,6 +5,7 @@ from kivy.uix.label import Label
 from kivy.uix.scrollview import ScrollView
 from kivy.uix.gridlayout import GridLayout
 from kivy.properties import ObjectProperty
+from kivy.clock import Clock
 import random
 import os
 
@@ -27,7 +28,7 @@ class MainScreen(Screen):
 
         scroll_view = ScrollView()
         scroll_view.add_widget(layout)
-    
+   
         self.add_widget(scroll_view)
 
     def on_button_press(self, number):
@@ -46,6 +47,10 @@ class BranchScreen(Screen):
         layout = GridLayout(cols=1, spacing=10, size_hint_y=None)
         layout.bind(minimum_height=layout.setter('height'))
 
+        back_btn=Button(text='뒤로가기', font_name=fontName, font_size=20, size_hint_y=None, height=40)
+        back_btn.bind(on_press=lambda x: self.back())
+        layout.add_widget(back_btn)
+
         for i in range(1, 51):
             btn = Button(text=f'Unit {i}', font_name=fontName, font_size=20, size_hint_y=None, height=40)
             btn.bind(on_press=lambda x, num=i: self.on_button_press(num))
@@ -53,18 +58,23 @@ class BranchScreen(Screen):
 
         scroll_view = ScrollView()
         scroll_view.add_widget(layout)
-    
+   
         self.add_widget(scroll_view)
 
     def on_button_press(self, number):
         global code
         code += number
-        print(code)
-        self.change_screen('word')
+        self.change_screen('word', 0)
 
-    def change_screen(self, screen_name):
-        self.manager.transition.direction = 'left'
-        self.manager.current = screen_name
+    def back(self):
+        self.change_screen('main', 1)
+
+    def change_screen(self, screen_name, num):
+        if num==0:
+            self.manager.transition.direction='left'
+        else:
+            self.manager.transition.direction='right'
+        self.manager.current=screen_name
 
 class WordScreen(Screen):
     app = ObjectProperty(None)
@@ -73,14 +83,15 @@ class WordScreen(Screen):
         super(WordScreen, self).__init__(**kwargs)
 
         self.layout = GridLayout(cols=2, spacing=10, padding=10)
-        self.wordLabel = Label(text="Label", font_name=fontName, font_size=20)
+        self.wordLabel = Label(text="Label", font_name=fontName, font_size=40)
         self.layout.add_widget(self.wordLabel)
-        self.layout.add_widget(Label())
+        self.timeLabel=Label(text='Time', font_name=fontName, font_size=40)
+        self.layout.add_widget(self.timeLabel)
 
-        self.btn1 = Button(text="1", font_name=fontName, font_size=20)
-        self.btn2 = Button(text="2", font_name=fontName, font_size=20)
-        self.btn3 = Button(text="3", font_name=fontName, font_size=20)
-        self.btn4 = Button(text="4", font_name=fontName, font_size=20)
+        self.btn1 = Button(text="1", font_name=fontName, font_size=30)
+        self.btn2 = Button(text="2", font_name=fontName, font_size=30)
+        self.btn3 = Button(text="3", font_name=fontName, font_size=30)
+        self.btn4 = Button(text="4", font_name=fontName, font_size=30)
 
         self.btn1.bind(on_press=lambda x: self.check_answer(self.btn1.text))
         self.btn2.bind(on_press=lambda x: self.check_answer(self.btn2.text))
@@ -93,12 +104,11 @@ class WordScreen(Screen):
         self.layout.add_widget(self.btn4)
 
         self.add_widget(self.layout)
-        self.load_words()
+        self.on_enter()
 
-    def load_words(self):
+    def on_enter(self):
         global code
-        print(code)
-        self.file_path = os.path.join(os.path.dirname(__file__), str(code) + '.txt')
+        self.file_path = os.path.join(os.path.dirname(__file__), 'WordFile', str(code) + '.txt')
         self.english_list = {}
         try:
             with open(self.file_path, 'r', encoding='utf-8') as file:
@@ -114,12 +124,13 @@ class WordScreen(Screen):
             self.wordLabel.text = '파일을 찾을 수 없습니다.'
 
     def show_word(self):
-        if len(set(self.word_list) - set(self.already_use)) == 0:
+        if len(self.word_list)==len(self.already_use):
             self.wordLabel.text = "모든 단어를 학습했습니다."
             self.btn1.text = ""
             self.btn2.text = ""
             self.btn3.text = ""
             self.btn4.text = ""
+            Clock.schedule_once(lambda dt: self.change_screen('main'), 2)
             return
 
         word = random.choice(list(set(self.word_list) - set(self.already_use)))
@@ -141,9 +152,63 @@ class WordScreen(Screen):
         self.btn4.text = possible_answers[3]
 
     def check_answer(self, selected_answer):
+        selected_button = None
         if selected_answer == self.correct_answer:
+            if self.btn1.text == self.correct_answer:
+                selected_button = self.btn1
+            elif self.btn2.text == self.correct_answer:
+                selected_button = self.btn2
+            elif self.btn3.text == self.correct_answer:
+                selected_button = self.btn3
+            elif self.btn4.text == self.correct_answer:
+                selected_button = self.btn4
+       
+        correct_button=None
+        if self.correct_answer==self.btn1.text:
+            correct_button=self.btn1
+        elif self.correct_answer==self.btn2.text:
+            correct_button=self.btn2
+        elif self.correct_answer==self.btn3.text:
+            correct_button=self.btn3
+        elif self.correct_answer==self.btn4.text:
+            correct_button=self.btn4
+
+        if selected_button is not None:
             self.show_word()
-        
+        else:
+            if self.btn1.text == selected_answer:
+                incorrect_button = self.btn1
+            elif self.btn2.text == selected_answer:
+                incorrect_button = self.btn2
+            elif self.btn3.text == selected_answer:
+                incorrect_button = self.btn3
+            elif self.btn4.text == selected_answer:
+                incorrect_button = self.btn4
+
+            self.disable_buttons()
+
+            correct_button.background_color = (0, 1, 0, 1)
+            incorrect_button.background_color = (1, 0, 0, 1)
+            Clock.schedule_once(lambda dt: self.init_color(correct_button, incorrect_button), 1)
+            Clock.schedule_once(lambda dt: self.enable_buttons(), 1)
+            Clock.schedule_once(lambda dt: self.show_word(), 1)
+       
+    def init_color(self, correct_button, incorrect_button):
+        correct_button.background_color = (1, 1, 1, 1)
+        incorrect_button.background_color = (1, 1, 1, 1)
+
+    def disable_buttons(self):
+        self.btn1.disabled = True
+        self.btn2.disabled = True
+        self.btn3.disabled = True
+        self.btn4.disabled = True
+   
+    def enable_buttons(self):
+        self.btn1.disabled = False
+        self.btn2.disabled = False
+        self.btn3.disabled = False
+        self.btn4.disabled = False
+
     def change_screen(self, screen_name):
         self.manager.transition.direction = 'left'
         self.manager.current = screen_name
